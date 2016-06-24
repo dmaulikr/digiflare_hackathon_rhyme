24d0564b20049316da9c0afa7c39e451d1203147
Sergii Gavryliuk <sergii.gavryliuk@digiflare.com>
2016-06-24 11:43:29 -0400

    Project added

---
+++ RhymeGame/RhymeGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RhymeGame
+//
+//  Created by Sergii Gavryliuk on 2016-06-24.
+//  Copyright © 2016 Sergey Gavrilyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
